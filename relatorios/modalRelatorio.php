5e2b7c1b4551cc952bc74fb206de163fdc7cb9c4
Marx Medeiros <analista.marx@gmail.com>
2016-10-28 11:56:00 -0200

    Correção

--- relatorios/modalRelatorio.php
+++ relatorios/modalRelatorio.php
@@ -57,17 +57,20 @@ process = function()
 <!-- MODAL DE CADASTRO DE FIRMWARE -->
 <!-- MODAL DE CADASTRO DE FIRMWARE -->
 <div id="ruserPeriodo" class="modal fade" role="dialog">
- <div class="modal-dialog">
+ <div class="modal-dialog modal-lg">
   <div class="modal-content">
-   <div class="modal-header bg-orange">
+   <div class="modal-header bg-primary">
     <button type="button" class="close" data-dismiss="modal">X</button>
-     <h4 class="modal-title">Relatório por Usuário</h4>
+     <h4 class="modal-title">Relatório por Usuário e período</h4>
    </div>
    <div class="modal-body">
    <?php 
+     $ChamaUsuario = "SELECT * FROM login WHERE Tipo='1'";
+     $Chu = $PDO->prepare($ChamaUsuario);
+     $Chu->execute();
    ?>
     <form action="UserPeriodo.php" method="get" name="ruserPeriodo" target="popup" onsubmit="process(); return false;">
-     <div class="col-xs-12">Modelo
+     <div class="col-xs-4">Usuário
       <select class="form-control" name="usuario" id="username" required="required">
        <option value="" selected="selected">SELECIONE</option>
        <?php while ($Chuu = $Chu->fetch(PDO::FETCH_ASSOC)): ?>
@@ -75,6 +78,22 @@ process = function()
        </option>
        <?php endwhile; ?>
       </select>
+     </div>
+     <div class="col-xs-4">Data Inicial
+      <div class="input-group">
+       <div class="input-group-addon">
+        <i class="fa fa-calendar"></i>
+       </div>
+        <input type="text" name="dtInicio" class="form-control" minlength="10" maxlength="10" OnKeyPress="formatar('##/##/####', this)" required="required">
+      </div>
+     </div>
+     <div class="col-xs-4">Data Final
+      <div class="input-group">
+       <div class="input-group-addon">
+        <i class="fa fa-calendar"></i>
+       </div>
+        <input type="text" name="dtFinal" class="form-control" minlength="10" maxlength="10" OnKeyPress="formatar('##/##/####', this)" required="required">
+      </div>
      </div>
      <div class="pull-right"><br />
       <input name="ruserPeriodo" type="submit" class="btn btn-success btn-flat" id="ruserPeriodo" value="Visualizar"  /> 
