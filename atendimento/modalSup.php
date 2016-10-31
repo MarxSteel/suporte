e96ec27fdb6891023b465f6d786488ebbf58326f
Marx Medeiros <analista.marx@gmail.com>
2016-10-31 16:08:23 -0200

    Revisão de Tabela

--- atendimento/modalSup.php
+++ atendimento/modalSup.php
@@ -122,9 +122,30 @@ $ChamaProduto = "SELECT * FROM produto";
      <h4 class="modal-title">AJUDA</h4>
    </div>
    <div class="modal-body">
-    NADA AQUI POR ENQUANTO ;)
-
+   <h3> Descrição dos Botões da área de suporte: </h3>
+   <div class="col-xs-4" align="center">BOTÃO</div>
+   <div class="col-xs-8" align="center">FUNÇÃO</div><br />
+   <div class="col-xs-4">
+    <button class="btn bg-navy btn-block"><i class="fa fa-reply-all"></i></button>
    </div>
+   <div class="col-xs-8" align="left"><h4>BOTÃO PARA REABRIR CHAMADO FINALIZADO</h4>
+   </div><br />
+   <div class="col-xs-4">
+    <button class="btn btn-default btn-block"><i class="fa fa-search"></i></button>
+   </div>
+   <div class="col-xs-8" align="left"><h4>BOTÃO PARA VISUALIZAR CHAMADO</h4>
+   </div><br />
+   <div class="col-xs-4">
+    <button class="btn bg-orange btn-block"><i class="fa fa-refresh"></i></button>
+   </div>
+   <div class="col-xs-8" align="left"><h4>BOTÃO PARA ATUALIZAR OBSERVAÇÕES DE CHAMADO</h4>
+   </div><br />
+   <div class="col-xs-4">
+    <button class="btn btn-success btn-block"><i class="fa fa-check"></i></button>
+   </div>
+   <div class="col-xs-8" align="left"><h4>BOTÃO PARA ATUALIZAR OBSERVAÇÕES DE CHAMADO</h4>
+   </div><br />
+
    <div class="modal-footer"></div>
   </div>
  </div>
