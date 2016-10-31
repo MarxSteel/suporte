409d1dfeee102b96624da2068f80a5a28a22b272
Marx Medeiros <analista.marx@gmail.com>
2016-10-31 10:07:02 -0200

    Revisões

--- atendimento/TPUser.php
+++ atendimento/TPUser.php
@@ -7,32 +7,32 @@ $PU->execute();
 <table id="pendente" class="table table-hover table-responsive">
  <thead>
   <tr>
-   <td width="5%">Chamado</td>
-   <td width="10%" >Modelo</td>
-   <td width="15%">Revenda</td>
-   <td width="15%">Técnico da Revenda</td>
-   <td width="30%" >Cadastro</td>
-   <td width="10%" >Solicitação</td>
-   <td width="15%"></td>
+   <td>Cham.</td>
+   <td>Modelo</td>
+   <td>Revenda</td>
+   <td>Técnico da Revenda</td>
+   <td>Cadastro</td>
+   <td>Atendente</td>
+   <td></td>
   </tr>
  </thead>
  <tbody>
   <?php while ($PUser = $PU->fetch(PDO::FETCH_ASSOC)): 
    echo '<tr>';
    echo '<td>' . $PUser["id"] . '</td>';
-   echo '<td><span class="badge bg-blue">' . $PUser["Equip"] . '</span></td>';
+   echo '<td>' . $PUser["Equip"] . '</td>';
    echo '<td>' . $PUser["Revenda"] . '</td>';
    echo '<td>' . $PUser["RevendaTecnico"] . '</td>';   
    echo '<td>' . $PUser["DescSolicita"] . '</td>';   
-   echo '<td>' . $PUser["DataCadastro"] . '</td>';
+   echo '<td>' . $PUser["UserAtendente"] . '</td>';
    echo '<td>';
-      echo '<a class="btn btn-default btn-sm" href="';
+      echo '<a class="btn btn-default btn-xs" href="';
       echo "javascript:abrir('Visualizar.php?ID=" . $PUser["id"] . "');";
       echo '"><i class="fa fa-search"></i></a>&nbsp;';  
-      echo '<a class="btn btn-warning btn-sm" href="';
+      echo '<a class="btn btn-warning btn-xs" href="';
       echo "javascript:abrir('Atualizar.php?ID=" . $PUser["id"] . "');";
       echo '"><i class="fa fa-refresh"></i></a>&nbsp;';    
-      echo '<a class="btn btn-success btn-sm" href="';
+      echo '<a class="btn btn-success btn-xs" href="';
       echo "javascript:abrir('Finaliza.php?ID=" . $PUser["id"] . "');";
       echo '"><i class="fa fa-check"></i></a>';    
    echo '</td>';
