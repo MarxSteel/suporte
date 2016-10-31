6e94fb37bb362150e45285f40770b2ae07bcc222
Marx Medeiros <analista.marx@gmail.com>
2016-10-31 10:06:56 -0200

    Revisões

--- atendimento/tabPendenteGeral.php
+++ atendimento/tabPendenteGeral.php
@@ -7,14 +7,13 @@ $PG->execute();
 <table id="tabPendenteGeral" class="table table-hover table-responsive">
  <thead>
   <tr>
-   <td width="5%">Chamado</td>
-   <td width="10%" >Modelo</td>
-   <td width="12%">Revenda</td>
-   <td width="15%">Técnico da Revenda</td>
-   <td width="15%">Técnico</td>
-   <td width="28%" >Cadastro</td>
-   <td width="10%" >Solicitação</td>
-   <td width="20%"></td>
+   <td>Chamado</td>
+   <td>Modelo</td>
+   <td>Revenda</td>
+   <td>Técnico da Revenda</td>
+   <td>Cadastro</td>
+   <td>Técnico Henry</td>
+   <td></td>
   </tr>
  </thead>
  <tbody>
@@ -24,20 +23,19 @@ $PG->execute();
    echo '<td><span class="badge bg-blue">' . $Pgeral["Equip"] . '</span></td>';
    echo '<td>' . $Pgeral["Revenda"] . '</td>';
    echo '<td>' . $Pgeral["RevendaTecnico"] . '</td>';   
-   echo '<td>' . $Pgeral["UserAtendente"] . '</td>';   
    echo '<td>' . $Pgeral["DescSolicita"] . '</td>';   
-   echo '<td>' . $Pgeral["DataCadastro"] . '</td>';
+   echo '<td>' . $Pgeral["UserAtendente"] . '</td>';
    $Atendente = $Pgeral["UserAtendente"];
    echo '<td>';
-    echo '<a class="btn btn-default btn-sm" href="';
+    echo '<a class="btn btn-default btn-xs" href="';
     echo "javascript:abrir('Visualizar.php?ID=" . $Pgeral["id"] . "');";
     echo '"><i class="fa fa-search"></i></a>&nbsp;';  
-    echo '<a class="btn btn-warning btn-sm" href="';
+    echo '<a class="btn btn-warning btn-xs" href="';
     echo "javascript:abrir('GAtualiza.php?ID=" . $Pgeral["id"] . "');";
     echo '"><i class="fa fa-refresh"></i></a>&nbsp;';
      if ($Atendente === $NomeUserLogado) 
      {
-      echo '<a class="btn btn-success btn-sm" href="';
+      echo '<a class="btn btn-success btn-xs" href="';
       echo "javascript:abrir('GFinaliza.php?ID=" . $Pgeral["id"] . "');";
       echo '"><i class="fa fa-check"></i></a>';    
      }
