e52382d0508f8797ae09912b9b8990b85f14c4e4
Marx Medeiros <analista.marx@gmail.com>
2016-11-02 19:49:07 -0200

    Revisão

--- init.php
+++ init.php
@@ -12,6 +12,17 @@ define('DB_PASS', $pass);
 define('DB_NAME', $banco);
 
 
+//DEFININDO BANCO 2
+
+$Host2 = "192.168.1.1:3306";
+$User2 = "marquistei";
+$Senha2 = "qaz654wsx";
+$Banco2 = "erp_henry";
+define('DB_HOST2', $Host2);
+define('DB_USER2', $User2);
+define('DB_PASS2', $Senha2);
+define('DB_NAME2', $Banco2);
+
 date_default_timezone_set('America/Sao_Paulo'); //DEFININDO O TIMEZONE PARA TODAS AS PÁGINAS
 
 
