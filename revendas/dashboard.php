ab83cd86836286e303057d8ae1c80e63db651f49
Marx Medeiros <analista.marx@gmail.com>
2016-11-02 19:49:25 -0200

    Revisão

--- revendas/dashboard.php
+++ revendas/dashboard.php
@@ -3,10 +3,12 @@ require("../restritos.php");
 require_once '../init.php';
 $cRev = "active";
 $PDO = db_connect();
+$PDO2 = db_connect2();
+
 require_once '../QueryUser.php';
 
-  $ChamaRevendas = "SELECT * FROM revenda";
-  $cRevenda = $PDO->prepare($ChamaRevendas);
+  $ChamaRevendas = "SELECT RAZAO_SOCIAL, CIDADE, EMAIL, DDD1, TELEFONE1, EMPRESA_ID FROM cad_empresa WHERE RAZAO_SOCIAL != ''";
+  $cRevenda = $PDO2->prepare($ChamaRevendas);
   $cRevenda->execute();
 ?>
 <!DOCTYPE html>
@@ -24,6 +26,14 @@ require_once '../QueryUser.php';
  <link rel="stylesheet" href="../dist/css/skins/_all-skins.min.css">
  <link rel="stylesheet" href="../plugins/iCheck/flat/blue.css">
  <link rel="stylesheet" href="../plugins/datatables/dataTables.bootstrap.css">
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+ <link rel="stylesheet" href="../dist/css/AdminLTE.min.css">
+ <link rel="stylesheet" href="../dist/css/skins/_all-skins.min.css">
+ <link rel="stylesheet" href="../plugins/iCheck/flat/blue.css">
+    <link rel="stylesheet" href="../plugins/select2/select2.min.css">
+ <link rel="stylesheet" href="../plugins/datatables/dataTables.bootstrap.css">
+
+
 </head>
 <body class="hold-transition skin-blue-light fixed sidebar-mini">
 <div class="wrapper">
@@ -81,42 +91,7 @@ require_once '../QueryUser.php';
        <h3 class="box-title">Lista de Revendas</h3>
      </div>
      <div class="box-body">
-      <table id="revenda" class="table table-hover table-striped">
-       <thead>
-        <tr>
-         <td>Razão Social</td>
-         <td>Nome Fantasia</td>
-         <td>Cidade</td>
-         <td>E-Mail</td>
-         <td width="15%">Telefone</td>
-         <td></td>
-        </tr>
-       </thead>
-       <tbody>
-        <?php while ($R = $cRevenda->fetch(PDO::FETCH_ASSOC)): 
-        echo '<tr>';
-         echo '<td>' . $R["RAZAO_SOCIAL"] . '</td>';
-         echo '<td>' . $R["NOME_FANTASIA"] . '</td>';
-         echo '<td>' . $R["CIDADE"] . '</td>';
-         echo '<td>' . $R["EMAIL"] . '</td>';
-         echo '<td>' . $R["DDD1"] . ' - ' . $R["TELEFONE1"] . '</td>';
-         echo '<td>';
-
-
-
-   
-      echo '<a class="btn btn-default btn-xs" href="';
-      echo "javascript:abrir('vRevenda.php?ID=" . $R["EMPRESA_ID"] . "');";
-      echo '"><i class="fa fa-search"></i></a>';  
-
-   echo '</td>';
-
-
-        echo '</tr>';
-        endwhile;
-        ?>    
-       </tbody>
-      </table>
+<table id="example" class="display" width="100%"></table>
 
 
 
@@ -130,11 +105,17 @@ require_once '../QueryUser.php';
  </section>
 </div><!-- CONTENT-WRAPPER -->
 <?php include_once '../footer.php'; ?>
-<script src="../plugins/jQuery/jquery-2.2.3.min.js"></script>
-<script src="../bootstrap/js/bootstrap.min.js"></script>
-<script src="../plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="../plugins/tabela/jquery-1.12.3.js"></script>
+<script src="../plugins/tabela/jquery.dataTables.min.js"></script>
+<script src="../plugins/tabela/dataTables.scroller.min.js"></script>
+
+
 <script src="../plugins/datatables/dataTables.bootstrap.min.js"></script>
-<script src="../plugins/slimScroll/jquery.slimscroll.min.js"></script>
+
+
+
+
+<script src="../bootstrap/js/bootstrap.min.js"></script>
 <script src="../plugins/fastclick/fastclick.js"></script>
 <script src="../dist/js/app.min.js"></script>
 <script src="../dist/js/demo.js"></script>
@@ -151,6 +132,112 @@ require_once '../QueryUser.php';
     });
   });
 </script>
+<script type="text/javascript">
+  var dataSet = [
+  {
+    "EMPRESA_ID": 1,
+    "BAIRRO": "VL PARANAGUA",
+    "CEP": 3806010,
+    "CIDADE": "SÃ£o Paulo",
+    "CNPJ": "4,85026E+13",
+    "COMPLEMENTO_END": "",
+    "DDD1": 11,
+    "DDD2": "",
+    "DDD3": "",
+    "DDD4": "",
+    "EMAIL": "comercial@massisrelogios.com.br",
+    "ENDERECO": "AVENIDA PARANAGUA",
+    "NOME_FANTASIA": "MASSIS TEC",
+    "NUMERO_END": 759,
+    "RAZAO_SOCIAL": "MASSIS TEC LTDA-ME",
+    "TELEFONE1": 25414400,
+    "TELEFONE2": "",
+    "TELEFONE3": "",
+    "TELEFONE4": "",
+    "UF": "SP",
+    "": ""
+  },
+  {
+    "EMPRESA_ID": "- Segue explicaÃ§Ã£o abaixo:",
+    "BAIRRO": "",
+    "CEP": "",
+    "CIDADE": "",
+    "CNPJ": "",
+    "COMPLEMENTO_END": "",
+    "DDD1": "",
+    "DDD2": "",
+    "DDD3": "",
+    "DDD4": "",
+    "EMAIL": "",
+    "ENDERECO": "",
+    "NOME_FANTASIA": "",
+    "NUMERO_END": "",
+    "RAZAO_SOCIAL": "",
+    "TELEFONE1": "",
+    "TELEFONE2": "",
+    "TELEFONE3": "",
+    "TELEFONE4": "",
+    "UF": "",
+    "": ""
+  }
+]
+
+
+  [
+    [ "Tiger Nixon", "System Architect", "Edinburgh", "5421", "2011/04/25", "$320,800" ],
+    [ "Garrett Winters", "Accountant", "Tokyo", "8422", "2011/07/25", "$170,750" ],
+    [ "Ashton Cox", "Junior Technical Author", "San Francisco", "1562", "2009/01/12", "$86,000" ],
+    [ "Cedric Kelly", "Senior Javascript Developer", "Edinburgh", "6224", "2012/03/29", "$433,060" ],
+    [ "Airi Satou", "Accountant", "Tokyo", "5407", "2008/11/28", "$162,700" ],
+    [ "Brielle Williamson", "Integration Specialist", "New York", "4804", "2012/12/02", "$372,000" ],
+    [ "Herrod Chandler", "Sales Assistant", "San Francisco", "9608", "2012/08/06", "$137,500" ],
+    [ "Rhona Davidson", "Integration Specialist", "Tokyo", "6200", "2010/10/14", "$327,900" ],
+    [ "Colleen Hurst", "Javascript Developer", "San Francisco", "2360", "2009/09/15", "$205,500" ],
+    [ "Sonya Frost", "Software Engineer", "Edinburgh", "1667", "2008/12/13", "$103,600" ],
+    [ "Jena Gaines", "Office Manager", "London", "3814", "2008/12/19", "$90,560" ],
+    [ "Quinn Flynn", "Support Lead", "Edinburgh", "9497", "2013/03/03", "$342,000" ],
+    [ "Charde Marshall", "Regional Director", "San Francisco", "6741", "2008/10/16", "$470,600" ],
+    [ "Haley Kennedy", "Senior Marketing Designer", "London", "3597", "2012/12/18", "$313,500" ],
+    [ "Tatyana Fitzpatrick", "Regional Director", "London", "1965", "2010/03/17", "$385,750" ],
+    [ "Michael Silva", "Marketing Designer", "London", "1581", "2012/11/27", "$198,500" ],
+    [ "Paul Byrd", "Chief Financial Officer (CFO)", "New York", "3059", "2010/06/09", "$725,000" ],
+    [ "Gloria Little", "Systems Administrator", "New York", "1721", "2009/04/10", "$237,500" ],
+    [ "Bradley Greer", "Software Engineer", "London", "2558", "2012/10/13", "$132,000" ],
+    [ "Dai Rios", "Personnel Lead", "Edinburgh", "2290", "2012/09/26", "$217,500" ],
+    [ "Jenette Caldwell", "Development Lead", "New York", "1937", "2011/09/03", "$345,000" ],
+    [ "Yuri Berry", "Chief Marketing Officer (CMO)", "New York", "6154", "2009/06/25", "$675,000" ],
+    [ "Caesar Vance", "Pre-Sales Support", "New York", "8330", "2011/12/12", "$106,450" ],
+    [ "Doris Wilder", "Sales Assistant", "Sidney", "3023", "2010/09/20", "$85,600" ],
+    [ "Angelica Ramos", "Chief Executive Officer (CEO)", "London", "5797", "2009/10/09", "$1,200,000" ],
+    [ "Gavin Joyce", "Developer", "Edinburgh", "8822", "2010/12/22", "$92,575" ],
+    [ "Jennifer Chang", "Regional Director", "Singapore", "9239", "2010/11/14", "$357,650" ],
+    [ "Brenden Wagner", "Software Engineer", "San Francisco", "1314", "2011/06/07", "$206,850" ],
+    [ "Fiona Green", "Chief Operating Officer (COO)", "San Francisco", "2947", "2010/03/11", "$850,000" ],
+    [ "Shou Itou", "Regional Marketing", "Tokyo", "8899", "2011/08/14", "$163,000" ],
+    [ "Michelle House", "Integration Specialist", "Sidney", "2769", "2011/06/02", "$95,400" ],
+    [ "Suki Burks", "Developer", "London", "6832", "2009/10/22", "$114,500" ],
+    [ "Prescott Bartlett", "Technical Author", "London", "3606", "2011/05/07", "$145,000" ],
+    [ "Gavin Cortez", "Team Leader", "San Francisco", "2860", "2008/10/26", "$235,500" ],
+    [ "Martena Mccray", "Post-Sales support", "Edinburgh", "8240", "2011/03/09", "$324,050" ],
+    [ "Unity Butler", "Marketing Designer", "San Francisco", "5384", "2009/12/09", "$85,675" ]
+];
+ 
+$(document).ready(function() {
+    $('#example').DataTable( {
+        data: dataSet,
+        columns: [
+            { title: "Name" },
+            { title: "Position" },
+            { title: "Office" },
+            { title: "Extn." },
+            { title: "Start date" },
+            { title: "Salary" }
+        ]
+    } );
+} );
+</script>
+
+
 <script language="JavaScript">
 function abrir(URL) { 
   var width = 1200;
