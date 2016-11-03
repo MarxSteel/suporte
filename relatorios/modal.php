cba75c1def1c594e5201f795ef344e600bc3f033
Marx Medeiros <analista.marx@gmail.com>
2016-11-03 16:49:06 -0200

    Correção

---
+++ relatorios/modal.php
@@ -0,0 +1,18 @@
+<!-- INÍCIO DE CADASTRO DOS MODALS DE REVENDA -->
+
+<!-- MODAL DE RELATORIO POR MODELO -->
+<div id="modalModelo" class="modal fade" role="dialog">
+ <div class="modal-dialog modal-lg">
+  <div class="modal-content">
+   <div class="modal-header bg-primary">
+    <button type="button" class="close" data-dismiss="modal">X</button>
+     <h4 class="modal-title">Relatório por Modelo</h4>
+   </div>
+   <div class="modal-body">
+
+   </div>
+   <div class="modal-footer"></div>
+  </div>
+ </div>
+</div>
+<!-- MODAL DE RELATORIO POR MODELO -->
