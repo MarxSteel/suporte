e00380abbc3947ee8b89911f685869a31e99b846
Marx Medeiros <analista.marx@gmail.com>
2016-11-04 10:54:42 -0200

    Revisões

--- relatorios/rRevendaGeral.php
+++ relatorios/rRevendaGeral.php
@@ -42,7 +42,7 @@ $teste = "teste";
 <head>
  <meta charset="utf-8">
  <meta http-equiv="X-UA-Compatible" content="IE=edge">
- <title>LiberaREP - Henry Equipamentos e Sistemas</title>
+ <title>Henry Equipamentos e Sistemas</title>
  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
  <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
