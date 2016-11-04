2c93a091d3496f0ccf3cd13b1ea9c1a939c91b9a
Marx Medeiros <analista.marx@gmail.com>
2016-11-04 11:08:10 -0200

    REVISADO

--- relatorios/modalUsuario.php
+++ relatorios/modalUsuario.php
@@ -55,14 +55,54 @@ process = function()
 
 
 <!-- MODAL REVENDA GERAL-->
-<div id="revendaPeriodo" class="modal fade" role="dialog">
+<div id="usuarioPeriodo" class="modal fade" role="dialog">
  <div class="modal-dialog modal-lg">
   <div class="modal-content">
-   <div class="modal-header bg-red">
+   <div class="modal-header bg-yellow">
     <button type="button" class="close" data-dismiss="modal">X</button>
-     <h4 class="modal-title">Relatório de revenda por período</h4>
+     <h4 class="modal-title">Relatório de usuário por período</h4>
    </div>
    <div class="modal-body">
+  <?php
+     $ChamaModelo2 = "SELECT nome FROM produto";
+     $P2 = $PDO->prepare($ChamaModelo2);
+     $P2->execute();
+  ?>
+    <form name="ruserper" action="rModeloPeriodo.php" target="_blank">
+
+     <div class="col-md-4">Modelo de Equipamento:
+      <div class="form-group">
+       <select class="form-control select2" name="modelo2" style="width: 100%;">
+        <option value="" selected="selected">SELECIONE</option>
+        <?php while ($p2 = $P2->fetch(PDO::FETCH_ASSOC)): ?>
+        <option value="<?php echo $p2['nome'] ?>"><?php echo $p2['nome'] ?></option>
+        <?php endwhile; ?>
+       </select>
+      </div>
+     </div>
+     <div class="col-xs-4">Data Inicial
+      <div class="input-group">
+       <div class="input-group-addon">
+        <i class="fa fa-calendar"></i>
+       </div>
+        <input type="text" name="dtInicio" class="form-control" minlength="10" maxlength="10" OnKeyPress="formatar('##/##/####', this)" required="required">
+      </div>
+     </div>
+     <div class="col-xs-4">Data Final
+      <div class="input-group">
+       <div class="input-group-addon">
+        <i class="fa fa-calendar"></i>
+       </div>
+        <input type="text" name="dtFinal" class="form-control" minlength="10" maxlength="10" OnKeyPress="formatar('##/##/####', this)" required="required">
+      </div>
+     </div>
+     <div class="col-xs-12"><br />
+      <input name="ruserper" type="submit" class="btn btn-warning btn-block btn-lg" value="Visualizar"  /> 
+     </div>
+    </form>
+
+
+
 
    </div>
    <div class="modal-footer"></div>
