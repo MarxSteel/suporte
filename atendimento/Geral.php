c4a36086cb09dbb5d1e0c74f5ba6ce1db5bb99d2
Marx Medeiros <analista.marx@gmail.com>
2016-11-09 13:25:41 -0200

    Revisão

--- atendimento/Geral.php
+++ atendimento/Geral.php
@@ -8,10 +8,9 @@ require_once '../QueryUser.php';
 <!DOCTYPE html>
 <html>
 <head>
-
- <meta charset="utf-8">
  <meta http-equiv="X-UA-Compatible" content="IE=edge">
  <title><?php echo $titulo; ?></title>
+ <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
  <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
@@ -68,7 +67,7 @@ require_once '../QueryUser.php';
   </aside>
 <div class="content-wrapper">
  <section class="content-header">
-  <h1>Suporte Técnico - Controle de Atendimentos Geral<small><?php echo $titulo; ?></small></h1>
+  <h1>Suporte T&eacute;cnico - Controle de Atendimentos Geral<small><?php echo $titulo; ?></small></h1>
  </section>
  <section class="content">
   <div class="row">
