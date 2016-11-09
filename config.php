f20f40edc285f9483538996bb54203505802f01f
Marx Medeiros <analista.marx@gmail.com>
2016-11-09 13:25:56 -0200

    Revisão

--- config.php
+++ config.php
@@ -3,6 +3,6 @@
 function dbcon()
 {
     @mysql_connect("localhost:8889", "root", "root") or die(mysql_error());
-    @mysql_select_db("producao") or die(mysql_error());
+    @mysql_select_db("henrySuporte") or die(mysql_error());
 }
 ?>
