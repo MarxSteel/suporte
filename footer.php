ead012934152302df136abbcff8800fc3bf8a45b
Marx Medeiros <analista.marx@gmail.com>
2016-11-09 13:26:29 -0200

    Revisão

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
   <footer class="main-footer">
     <div class="pull-right hidden-xs">
-      <b>Versão</b> <?php echo $versao; ?>
+      <b>Vers&atilde;o</b> <?php echo $versao; ?>
     </div>
     <strong>Copyright &copy; 2016 <a href="http://henry.com.br">Henry Equipamentos e Sistemas </a>.</strong> Todos os Direitos Reservados.
   </footer>
