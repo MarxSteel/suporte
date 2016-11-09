3815a14d1bed46957be283299402f9f07a26b9a0
Marx Medeiros <analista.marx@gmail.com>
2016-11-09 13:26:41 -0200

    Revisão

--- menuLateral.php
+++ menuLateral.php
@@ -1,41 +1,1 @@
-<?php 
-$server = 'http://localhost:8888/suporte';
-$endereco = $_SERVER ['SERVER_ADDR']; 
-?>
-<ul class="sidebar-menu">
- <li class="header"></li>
-  <li class="<?php echo $cHome; ?>">
-   <a href="<?php echo $server; ?>/dashboard.php">
-    <i class="fa fa-home"></i> <span>Início</span>
-   </a>
-  </li>
-  <?php if ($permSup === "1") { ?>  
-  <li class="<?php echo $cAtend; ?>">
-   <a href="<?php echo $server; ?>/atendimento/dashboard.php">
-    <i class="fa fa-plus"></i> <span>Atendimentos</span>
-   </a>
-  </li>
-  <li class="<?php echo $cRev; ?>">
-   <a href="<?php echo $server; ?>/revendas/dashboard.php">
-    <i class="fa fa-newspaper-o"></i> <span>Cadastro de Revendas</span>
-   </a>
-  </li>
-  <?php } else { } if ($permFw === "1") { ?>
-  <li class="<?php echo $cDoc; ?>">
-   <a href="<?php echo $server; ?>/engenharia/dashboard.php">
-    <i class="fa fa-file-code-o"></i> <span>Documentação</span>
-   </a>
-  </li>
-  <?php } else { } if ($permRel === "1") { ?>
-  <li class="<?php echo $cRel; ?>">
-   <a href="<?php echo $server; ?>/relatorios/dashboard.php">
-    <i class="fa fa-pie-chart"></i> <span>Relatório</span>
-   </a>
-  </li>
-  <?php } else { } if ($permUsr === "1") { ?>
-  <li class="<?php echo $cUser; ?>">
-   <a href="<?php echo $server; ?>/usuarios/dashboard.php">
-    <i class="fa fa-users"></i> <span>Usuários</span>
-   </a>
-  </li>
-  <?php } else { } ?>
+<?php $server = 'http://192.168.1.1/sistema/suporte';$endereco = $_SERVER ['SERVER_ADDR']; ?><ul class="sidebar-menu"> <li class="header"></li>  <li class="<?php echo $cHome; ?>">   <a href="<?php echo $server; ?>/dashboard.php">    <i class="fa fa-home"></i> <span>In&iacute;cio</span>   </a>  </li>  <?php if ($permSup === "1") { ?>    <li class="<?php echo $cAtend; ?>">   <a href="<?php echo $server; ?>/atendimento/dashboard.php">    <i class="fa fa-plus"></i> <span>Atendimentos</span>   </a>  </li>  <li class="<?php echo $cRev; ?>">   <a href="<?php echo $server; ?>/revendas/dashboard.php">    <i class="fa fa-newspaper-o"></i> <span>Cadastro de Revendas</span>   </a>  </li>  <?php } else { } if ($permFw === "1") { ?>  <li class="<?php echo $cDoc; ?>">   <a href="<?php echo $server; ?>/engenharia/dashboard.php">    <i class="fa fa-file-code-o"></i> <span>Cadastro de Equipamentos</span>   </a>  </li>  <?php } else { } if ($permRel === "1") { ?>  <li class="<?php echo $cRel; ?>">   <a href="<?php echo $server; ?>/relatorios/dashboard.php">    <i class="fa fa-pie-chart"></i> <span>Relat&oacute;rio</span>   </a>  </li>  <?php } else { } if ($permUsr === "1") { ?>  <li class="<?php echo $cUser; ?>">   <a href="<?php echo $server; ?>/usuarios/dashboard.php">    <i class="fa fa-users"></i> <span>Usu&aacute;rios</span>   </a>  </li>  <?php } else { } ?>
